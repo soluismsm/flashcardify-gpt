44c8c042c1f1edc35ca32bc98ee37fcf6292a5ee
Luis Miguel <vluismw@gmail.com>
2023-03-31 08:29:22 -0300

    feat: Add user inputs

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 START_RANGE = 1 << 30
 STOP_RANGE = 1 << 31
+DEFAULT_DECK_NAME = "Frases Cotidianas em Espanhol"
 
 # Put your API key here
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -22,19 +23,12 @@ def chat_completion(message):
 
 
 def format_reply(content):
-    # [ ] todo: trocar o for loop por list comprehension
     try:
         reply = content.split("\n")
-        # reply = [v.replace(f"{i+1}) ", "") for i, v in enumerate(reply) if "1)" in v]
-        # reply = [v.replace(f"{i+1}. ", "") for i, v in enumerate(reply) if "1." in v]
-        # print(reply)
-        for i, v in enumerate(reply):
-            if "1)" in v:
-                reply[i] = v.replace(f"{i+1}) ", "")
-            else:
-                reply[i] = v.replace(f"{i+1}. ", "")
-        card = reply[i].split("|")
-        reply[i] = card
+        reply = [
+            v.replace(f"{i+1}) ", "").replace(f"{i+1}. ", "").split("|")
+            for i, v in enumerate(reply)
+        ]
         return reply
     except Exception as e:
         print(e)
@@ -69,27 +63,37 @@ def create_model(deck_id):
     return model
 
 
-def create_deck(deck_id, deck_name="Frases Cotidianas em Espanhol", deck_quantity=50):
+def create_deck(deck_id, deck_name, deck_quantity):
     deck = genanki.Deck(deck_id, f"{deck_quantity} {deck_name}")
     return deck
 
 
+def get_deck_size(default_size=50):
+    while True:
+        try:
+            deck_size = input("Quantidade de Notas (Padrão: 50): ")
+            if deck_size == "":
+                return default_size
+            return int(deck_size)
+        except ValueError:
+            print("Digite um número válido!")
+
+
 def main():
-    # Modify the deck name
-    # [ ] todo: Mudar o deck_name padrão que create_deck() recebe
-    deck_name = input("Nome do Deck: ")
-    deck_quantity = int(
-        input("Digite a quantidade de Notas que gostaria de gerar (Padrão: 50): ")
-    )
+    deck_name = input(f"Nome do Deck (Default: '{DEFAULT_DECK_NAME}'): ")
+    if deck_name == "":
+        deck_name = DEFAULT_DECK_NAME
+    deck_size = get_deck_size()
+
     # Customize flashcard content
-    message = f"""Faça {deck_quantity} Flashcards com frases cotidianas em espanhol para
+    message = f"""Faça {deck_size} Flashcards com frases cotidianas em espanhol para
     português seguindo essa formatação: [concept] | [answer]."""
 
     content = chat_completion(message)
     reply = format_reply(content)
     deck_id = generate_deck_id()
     model = create_model(deck_id)
-    deck = create_deck(deck_id, deck_name, deck_quantity)
+    deck = create_deck(deck_id, deck_name, deck_size)
     for note in reply:
         note = create_note(model, note)
         deck.add_note(note)
