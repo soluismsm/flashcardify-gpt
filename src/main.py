09e95c4a4ed3d61cda62781c86c86ee262f8dbcf
soluismsm <vluismw@gmail.com>
2023-04-04 17:51:29 -0300

    refactor: :recycle: Refactor code

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from openai_helper import get_chat_response
 START_RANGE = 1 << 30
 STOP_RANGE = 1 << 31
 DEFAULT_DECK_NAME = "Frases Cotidianas em Espanhol"
+DEFAULT_DECK_SIZE = 3
 
 
 def format_reply(content):
@@ -34,29 +35,40 @@ def generate_deck_id():
     return deck_id
 
 
-def get_deck_size(default_size=3):
-    """Pergunta ao usuário o tamanho do deck que quer gerar."""
+def get_deck_size():
+    """Get from user the deck size, if empty, use default value"""
 
     while True:
         try:
-            deck_size = input(f"Quantidade de Notas (Padrão: {default_size}): ")
+            deck_size = input(f"Quantidade de Notas (Padrão: {DEFAULT_DECK_SIZE}): ")
             if deck_size == "":
-                return default_size
+                return DEFAULT_DECK_SIZE
             return int(deck_size)
         except ValueError:
             print("Digite um número válido!")
 
 
+def get_deck_name():
+    """Get from user which name he wants to give to the deck."""
+
+    while True:
+        try:
+            deck_name = input(f"Deck Name (Default: '{DEFAULT_DECK_NAME}'): ")
+            if deck_name == "":
+                return DEFAULT_DECK_NAME
+            return deck_name
+        except ValueError:
+            print("Digite um nome válido!")
+
+
 def main():
-    deck_name = input(f"Nome do Deck (Default: '{DEFAULT_DECK_NAME}'): ")
-    if deck_name == "":
-        deck_name = DEFAULT_DECK_NAME
+    deck_name = get_deck_name()
     deck_size = get_deck_size()
 
     # Change this if you want to customize flashcard content
     chat_message = (
-        f"Crie {deck_size} Flashcards com sentenças em espanhol para português"
-        "seguindo essa formatação: [concept] | [answer]."
+        f"Crie {deck_size} Flashcards com sentenças em espanhol seguindo essa "
+        "formatação: [concept] | [answer]."
     )
 
     content = get_chat_response(chat_message)
